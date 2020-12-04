e1c8f636a3bd3d43b5dfbceb66b57802ef8ba0ba
Sachin Dehury <51115041+Sachin2Dehury@users.noreply.github.com>
2020-12-04 21:31:50 +0530

    Working on ui

--- app/src/main/java/github/sachin2dehury/musicplayer/adapters/SongAdapter.kt
+++ app/src/main/java/github/sachin2dehury/musicplayer/adapters/SongAdapter.kt
@@ -1,4 +1,30 @@
 package github.sachin2dehury.musicplayer.adapters
 
-class SongAdapter {
+import androidx.recyclerview.widget.AsyncListDiffer
+import com.bumptech.glide.RequestManager
+import github.sachin2dehury.musicplayer.R
+import kotlinx.android.synthetic.main.list_item.view.*
+import javax.inject.Inject
+
+class SongAdapter @Inject constructor(
+    private val glide: RequestManager
+) : BaseSongAdapter(R.layout.list_item) {
+
+    override val differ = AsyncListDiffer(this, diffCallback)
+
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            textViewPrimary.text = song.title
+            textViewSecondary.text = song.subTitle
+            glide.load(song.imageUrl).into(imageViewItem)
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/github/sachin2dehury/musicplayer/adapters/SwipeSongAdapter.kt
+++ app/src/main/java/github/sachin2dehury/musicplayer/adapters/SwipeSongAdapter.kt
@@ -1,4 +1,24 @@
 package github.sachin2dehury.musicplayer.adapters
 
-class SwipeSongAdapter {
+import androidx.recyclerview.widget.AsyncListDiffer
+import github.sachin2dehury.musicplayer.R
+import kotlinx.android.synthetic.main.swipe_item.view.*
+
+class SwipeSongAdapter : BaseSongAdapter(R.layout.swipe_item) {
+
+    override val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            val text = "${song.title} - ${song.subTitle}"
+            textViewPrimary.text = text
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
 }
