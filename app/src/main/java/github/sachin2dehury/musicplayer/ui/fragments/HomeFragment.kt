c9d85695e492bb6e4aae3828771f7c7e682144cb
Sachin Dehury <51115041+Sachin2Dehury@users.noreply.github.com>
2020-12-04 22:43:16 +0530

    Working on ui

--- app/src/main/java/github/sachin2dehury/musicplayer/ui/fragments/HomeFragment.kt
+++ app/src/main/java/github/sachin2dehury/musicplayer/ui/fragments/HomeFragment.kt
@@ -1,7 +1,25 @@
 package github.sachin2dehury.musicplayer.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import github.sachin2dehury.musicplayer.R
+import github.sachin2dehury.musicplayer.adapters.SongAdapter
+import github.sachin2dehury.musicplayer.ui.viewmodels.MainViewModel
+import javax.inject.Inject
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var songAdapter: SongAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+    }
 }
--- app/src/main/java/github/sachin2dehury/musicplayer/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/github/sachin2dehury/musicplayer/ui/viewmodels/MainViewModel.kt
@@ -1,4 +1,85 @@
 package github.sachin2dehury.musicplayer.ui.viewmodels
 
-class MainViewModel {
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import github.sachin2dehury.musicplayer.data.entities.Song
+import github.sachin2dehury.musicplayer.exoplayer.MusicServiceConnection
+import github.sachin2dehury.musicplayer.exoplayer.isPlayEnabled
+import github.sachin2dehury.musicplayer.exoplayer.isPlaying
+import github.sachin2dehury.musicplayer.exoplayer.isPrepared
+import github.sachin2dehury.musicplayer.others.Constants.MEDIA_ROOT_ID
+import github.sachin2dehury.musicplayer.others.Resource
+
+class MainViewModel @ViewModelInject constructor(
+    private val musicServiceConnection: MusicServiceConnection
+) : ViewModel() {
+
+    private val _mediaItems = MutableLiveData<Resource<List<Song>>>()
+    val mediaItems: LiveData<Resource<List<Song>>> = _mediaItems
+
+    val isConnected = musicServiceConnection.isConnected
+    val networkError = musicServiceConnection.networkError
+    val currentSongPlaying = musicServiceConnection.currentSongPlaying
+    val playbackState = musicServiceConnection.playbackState
+
+    init {
+        _mediaItems.postValue(Resource.loading(null))
+        musicServiceConnection.subscribe(MEDIA_ROOT_ID,
+            object : MediaBrowserCompat.SubscriptionCallback() {
+                override fun onChildrenLoaded(
+                    parentId: String,
+                    children: MutableList<MediaBrowserCompat.MediaItem>
+                ) {
+                    super.onChildrenLoaded(parentId, children)
+
+                    val items = children.map { song ->
+                        Song(
+                            song.mediaId!!,
+                            song.description.title.toString(),
+                            song.description.subtitle.toString(),
+                            song.description.mediaUri.toString(),
+                            song.description.iconUri.toString()
+                        )
+                    }
+                    _mediaItems.postValue(Resource.success(items))
+                }
+            })
+    }
+
+    fun skipToNextSong() {
+        musicServiceConnection.transportControls.skipToNext()
+    }
+
+    fun skipToPreviousSong() {
+        musicServiceConnection.transportControls.skipToPrevious()
+    }
+
+    fun seekTo(position: Long) {
+        musicServiceConnection.transportControls.seekTo(position)
+    }
+
+    fun playOrToggleSong(song: Song, toggle: Boolean = false) {
+        val isPrepared = playbackState.value?.isPrepared ?: false
+        if (isPrepared && song.mediaId == currentSongPlaying.value?.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID)) {
+            playbackState.value?.let { playbackState ->
+                when {
+                    playbackState.isPlaying -> if (toggle) musicServiceConnection.transportControls.pause()
+                    playbackState.isPlayEnabled -> musicServiceConnection.transportControls.play()
+                    else -> Unit
+                }
+            }
+        } else {
+            musicServiceConnection.transportControls.playFromMediaId(song.mediaId, null)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        musicServiceConnection.unsubscribe(MEDIA_ROOT_ID,
+            object : MediaBrowserCompat.SubscriptionCallback() {})
+    }
 }
